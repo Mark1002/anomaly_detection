0dc2fa303dff6887ca8663e9a3bc90f3df38e5dc
mark-PC\mark <ak0922747401@gmail.com>
2017-12-28 17:28:57 +0800

    refactor pca to pca moudle

--- anomal_algo/pca.py
+++ anomal_algo/pca.py
@@ -4,6 +4,49 @@ Created on Wed Dec 27 15:02:24 2017
 
 @author: mark
 """
+import numpy as np
+import numpy.linalg as ln
 
 class PCA:
-    pass
+    def __init__(self):
+        self.eig_values = None
+        self.eig_vectors = None
+        self.is_fit = False
+        
+    def fit(self, X):
+        cov_matrix = np.cov(X.T)  
+        self.eig_values, self.eig_vectors = ln.eig(cov_matrix)
+        self.is_fit = True
+    
+    def get_params(self):
+        if not self.is_fit:
+            raise ValueError("PCA is not fit!")
+        params_dict = {
+            'eig_values': self.eig_values, 
+            'eig_vectors': np.matrix(self.eig_vectors)
+        }
+        return params_dict
+    
+    def get_pca_component(self, percent):
+        if not self.is_fit:
+            raise ValueError("PCA is not fit!")
+        for i in range(len(self.eig_values)):
+            if sum(self.eig_values[0:i+1])/sum(self.eig_values) > percent:
+                return i + 1 
+
+    def perform_t_squared(self, X, V):
+        if not self.is_fit:
+            raise ValueError("PCA is not fit!")
+        X = np.matrix(X)
+        V = np.matrix(V)
+        components = V.shape[1]
+        eiv_diag_matrix=np.matrix(ln.inv(np.diag(self.eig_values[0:components])))
+        value = X*V*eiv_diag_matrix*V.T*X.T
+        return value.item()
+ 
+    def perform_reconstruction_error(self, X, V):
+        if not self.is_fit:
+            raise ValueError("PCA is not fit!")
+        X = np.matrix(X)
+        V = np.matrix(V)
+        return ln.norm((X - X*V*V.T))
--- anomaly_detection.py
+++ anomaly_detection.py
@@ -6,24 +6,11 @@ Created on Fri Dec 15 17:12:00 2017
 """
 import pandas as pd
 import numpy as np
-import numpy.linalg as ln
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from data_preprocess.time_data import TimeDataPreprocess
+from anomal_algo.pca import PCA
 
-def get_pca_component(eign_values, percent):
-    for i in range(len(eign_values)):
-        if sum(eign_values[0:i+1])/sum(eign_values) > percent:
-            return i + 1 
-
-def perform_t_squared(X, V):
-    components = V.shape[1]
-    eiv_diag_matrix=np.matrix(ln.inv(np.diag(eig_value[0:components])))
-    value = X*V*eiv_diag_matrix*V.T*X.T
-    return value.item()
- 
-def perform_reconstruction_error(X, V):
-    return ln.norm((X - X*V*V.T))
 
 def perform_normalize(vectors):
     vectors = np.array(vectors)
@@ -58,37 +45,26 @@ scale_train_window = scaler.transform(train_data_window)
 scale_test_window = scaler.transform(test_data_window)
 
 # PCA compute
-cov_matrix = np.cov(scale_train_window.T)  
-eig_value, eig_vectors = ln.eig(cov_matrix)
-
+pca = PCA()
+pca.fit(scale_train_window)
+pca_params = pca.get_params()
+eig_vectors = pca_params['eig_vectors']
 # 取得 95% 重要的前幾個主成分
-components = get_pca_component(eig_value, 0.95)
-
-anomaly_score_list = []
-anomaly_score_list2 = []
-scale_test_window = np.matrix(scale_test_window)
-eig_vectors = np.matrix(eig_vectors)
-
+components = pca.get_pca_component(0.95)
 V = eig_vectors[:,0:components]
 
+anomaly_score_list = []
 for num in range(len(scale_test_window)):
-    # can not understand
-    anomaly_score = perform_t_squared(scale_test_window[num], V)
-    anomaly_score2 = perform_reconstruction_error(scale_test_window[num], V)
+    anomaly_score = pca.perform_reconstruction_error(scale_test_window[num], V)
     anomaly_score_list.append(anomaly_score)
-    anomaly_score_list2.append(anomaly_score2)
-   
 
 anomaly_score_list = perform_normalize(anomaly_score_list)
-anomaly_score_list2 = perform_normalize(anomaly_score_list2)
-     
+  
 plt.figure(1)
-plt.subplot(411)
+plt.subplot(311)
 plt.title('origin time series')
 plt.plot(test_series.values)
-plt.subplot(413)
+plt.subplot(313)
 plt.title('anomaly_score')
 plt.plot(anomaly_score_list)
-plt.subplot(414)
-plt.plot(anomaly_score_list2)
 plt.show()
