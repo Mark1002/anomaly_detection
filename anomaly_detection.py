c0c3d036262fda8d212cb3098f56ada9dba3233a
mark-PC\mark <ak0922747401@gmail.com>
2018-01-16 18:07:41 +0800

    change figure plot

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -11,19 +11,23 @@ from sklearn.preprocessing import StandardScaler
 from data_preprocess.time_data import TimeDataPreprocess
 from anomal_algo.pca import PCA
 
-
-def perform_normalize(vectors):
-    vectors = np.array(vectors)
-    return vectors/(max(vectors)-min(vectors))
-
 # train data
 train_data = pd.read_csv("CSV/fan_data/fan1.csv")
 train_series = pd.Series(train_data['OLACTIVEPOWER'].values, 
                          name="OLACTIVEPOWER", 
                          index=pd.to_datetime(train_data['REPORTTIME']))
 # 選擇弱風速
-train_series = train_series[9:389]
-train_data_window = TimeDataPreprocess.transform_time_window(train_series, 10, 1, True)
+train_series = train_series.sort_index()
+train_series = train_series['2016-09-20 16:40':'2016-09-21 09:48']
+
+# drop na
+train_series[train_series == 0] = np.nan
+train_series = train_series.dropna()
+
+# regular time series interval to 3 min
+train_series_regular = train_series.resample('3T').bfill()
+train_series_regular = train_series_regular.interpolate(method='time', 
+                                                        limit_direction='both')
 
 # test data
 test_data = pd.read_csv("CSV/fan_data/fan1_error.csv")
@@ -34,10 +38,15 @@ test_series = pd.Series(test_data['OLACTIVEPOWER'].values,
 test_series = test_series[::-1]
 abnormal_series1 = list(test_series[:'2016-10-06 14:48'])
 abnormal_series2 = list(test_series['2016-10-06 15:10':])
-test_experiment_series = test_series['2016-10-06 14:24':'2016-10-06 15:35']
+test_series = test_series['2016-10-06 14:24':'2016-10-06 15:35']
+test_series = test_series.resample('3T').bfill()
+# test_series = pd.Series(abnormal_series1 + list(np.random.uniform(50, 51, size=60)) + abnormal_series2)
 
-test_series = pd.Series(abnormal_series1 + list(np.random.uniform(50, 51, size=60)) + abnormal_series2)
-test_data_window = TimeDataPreprocess.transform_time_window(test_series, 10, 1, True)
+# shift to time window
+train_data_window = TimeDataPreprocess.transform_time_window(train_series, 10, 1)
+train_data_window = train_data_window.apply(TimeDataPreprocess.extract_feature, axis=1)
+test_data_window = TimeDataPreprocess.transform_time_window(test_series, 10, 1)
+test_data_window = test_data_window.apply(TimeDataPreprocess.extract_feature, axis=1)
 
 # 標準化
 scaler = StandardScaler()
@@ -62,10 +71,11 @@ for num in range(len(scale_test_window)):
 anomaly_score_list = perform_normalize(anomaly_score_list)
   
 plt.figure(1)
-plt.subplot(311)
-plt.title('origin time series')
-plt.plot(test_series.values)
-plt.subplot(313)
+plt.subplot(211)
 plt.title('anomaly_score')
 plt.plot(anomaly_score_list)
+plt.subplot(212)
+plt.title('origin time series')
+plt.plot(test_series.values[9:])
+plt.tight_layout()
 plt.show()
--- light_data_preprocess.py
+++ light_data_preprocess.py
@@ -19,6 +19,10 @@ def remove_na_zero(df):
     df = df[ts[ts.values].index]
     return df
 
+def normalize(vectors):
+    vectors = np.array(vectors)
+    return (vectors-vectors.min())/(vectors.max()-vectors.min())
+    
 def perform_threshold_predict(anomaly_score_series, threshold):
     predict_label = (anomaly_score_series > threshold).values
     return predict_label
@@ -37,7 +41,7 @@ train_series = train_series.dropna()
 # remove outlier
 train_series = TimeDataPreprocess.remove_outlier(train_series, 1)
 # regular time series interval to 1 min
-train_series_regular = train_series.resample('1T').asfreq()
+train_series_regular = train_series.resample('1T').bfill()
 train_series_regular = train_series_regular.interpolate(method='time', 
                                                         limit_direction='both')
 # random test series
@@ -59,7 +63,7 @@ ground_true_label = np.array(
 train_X = train_df.loc[:,(train_df.columns != 't')]
 train_Y = train_df['t'].values
 
-train_df_fe = (train_df.iloc[0:6000,]).apply(TimeDataPreprocess.extract_feature, 
+train_df_fe = (train_df.iloc[0:60000,]).apply(TimeDataPreprocess.extract_feature, 
                axis=1)
 test_df_fe = test_df.apply(TimeDataPreprocess.extract_feature, axis=1)
 
@@ -83,14 +87,28 @@ for num in range(len(scale_test_window)):
     anomaly_score = pca.perform_reconstruction_error(scale_test_window[num], V)
     anomaly_score_list.append(anomaly_score)
 
-anomaly_score_series = pd.Series(anomaly_score_list, name="recon_error", 
+anomaly_score_series = pd.Series(normalize(anomaly_score_list), name="recon_error", 
                                  index=test_df_fe.index)
 
-plt.subplot(211)
-plt.plot(anomaly_score_series)
-plt.subplot(212)
-plt.plot(test_series)
+fig, ax1 = plt.subplots(figsize=(10, 5))
+x_axis = anomaly_score_series.index
+y1 = test_series[9:].values
+y2 = anomaly_score_series.values
+line1 = ax1.plot(x_axis, y1, 'b-', label='W')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('W', color='b')
+ax1.tick_params('y', colors='b')
+ax2 = ax1.twinx()
+line2 = ax2.plot(x_axis, y2, 'r-', label='Anomaly score')
+ax2.set_ylabel('Anomaly score', color='r')
+ax2.tick_params('y', colors='r')
+line3 = [ax2.axhline(y=0.1, color='g', linestyle='--', label='Threshold')]
+lines = line1 + line2 + line3
+labels = [l.get_label() for l in lines]
+ax2.legend(lines, labels)
+fig.tight_layout()
 plt.show()
+
 
 predict_label = perform_threshold_predict(anomaly_score_series, 50)
 con_matrix = confusion_matrix(ground_true_label, predict_label)
